2ca0da8eaa98aa87b3a71f23b473781604433d03
wintaatsbhaa <atsbhawinta5@gmail.com>
2024-12-28 00:57:07 -0900

    second commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GroupMembers from './GroupMembers';
+
+function App() {
+  return (
+    <div className="App">
+      <GroupMembers />
+    </div>
+  );
+}
+
+export default App;
